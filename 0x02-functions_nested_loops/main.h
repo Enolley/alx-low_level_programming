582f6bb30122d3401c7c0429dde48d790e910a6f
Enolley <85625222+Enolley@users.noreply.github.com>
2022-09-13 14:38:59 +0300

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*
- * File: main.h
- * Auth: Jan Nelson
- * Desc: Header file containing declarations for all functions
- *       used in the 0x02-functions_nested_loops directory.
- */
 
 int _putchar(char c);
 void print_alphabet(void);
