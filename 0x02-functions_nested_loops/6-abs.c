dd8fc9bd7bd1297fbe30dfee0431bb7f290bf528
Enolley <enolaomukamani@gmail.com>
2022-09-13 06:07:45 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _abs - absolute value printer
+* @n: number under check
+*
+* Return: Always 0
+*/
+
+int _abs(int)
+{
+if (n > 0)
+{
+return (n);
+}
+else
+{
+return (-n)
+}
+}
