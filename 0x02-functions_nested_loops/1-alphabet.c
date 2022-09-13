eb304c3be5abe8504f88d75b62f9b0604f88010f
Enolley <85625222+Enolley@users.noreply.github.com>
2022-09-13 14:50:24 +0300

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,12 +8,10 @@
 
 void print_alphabet(void)
 {
-  char lower;
-
-  for (lower = 'a'; lower <= 'z'; lower++)
-  {
-    _putchar(lower);
-  }
-  
-  _putchar('\n');
+char lower;
+for (lower = 'a'; lower <= 'z'; lower++)
+{
+_putchar(lower);
+}
+_putchar('\n');
 }
