b53b570e1b27c4c368607bd3f290131a8020d25d
Enolley <enolaomukamani@gmail.com>
2022-09-13 05:53:11 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_sign - sign printing
+* @n: number to be checked
+*
+* Return: 1 for positive, 0 for neutral, -1 for negative
+*/
+
+int print_sign(int n)
+{
+if (n > 0)
+return (1);
+_putchar('+');
+else if (n < 0)
+return (-1);
+_putchar('-');
+else
+return (0);
+_putchar('0);
+}
