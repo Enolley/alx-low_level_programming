74cfe48d2afefa7f98bd36e1991ff2da59970a31
Enolley <enolaomukamani@gmail.com>
2022-09-13 05:21:12 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _islower - checks if there is lowercase
+*\
+*
+* Return: Always 0
+*
+*/
+
+int _islowe(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+else 
+return (0);
+}
