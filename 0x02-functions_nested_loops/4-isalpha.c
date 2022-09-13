94705da2d410db225a96eea361639a7b07e032ad
Enolley <enolaomukamani@gmail.com>
2022-09-13 05:37:07 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isalpha - uppercase and lowercase
+* @c: character to be checked
+*
+* Return: Always 0
+*/
+
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z' || c>= 'A' && c <= 'Z')
+return (0);
+else
+return (1);
+}
