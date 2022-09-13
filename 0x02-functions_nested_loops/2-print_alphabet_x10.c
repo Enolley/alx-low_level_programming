9a08c908d5db87363cebcd5a4b2e926c2b7cccd5
Enolley <enolaomukamani@gmail.com>
2022-09-13 05:04:08 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - Prints 10 times
+*
+* Return: Always 0
+*/
+
+void print_alphabet_x10(void)
+{
+int count = 0;
+char lower
+;
+
+while(count++ <= 9)
+{
+for (lower = 'a'; lower <= 'z'; lower++)
+{
+_putchar(lower);
+}
+_putchar('\n');
+}
+}
