89708a00b8cd7b6ecaa303ed8afaa1c679add224
Enolley <enolaomukamani@gmail.com>
2022-09-13 04:34:06 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main - Print alphabet
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+char lower;
+
+for (lower = 'a'; lower <= 'z'; lower++)
+{
+_putchar(lower);
+}
+_putchar('\n');
+return (0);
+}
