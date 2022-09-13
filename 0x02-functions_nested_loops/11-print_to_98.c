43df2219ad2901d95841eebfc5a0253db593345b
Enolley <85625222+Enolley@users.noreply.github.com>
2022-09-13 16:57:26 +0300

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * add - Adds two integers.
+ * print_to_98 - Adds two integers.
  * @num1: The first integer to be added.
  * @num2: The second integer to be added.
  *
  * Return: The result of the addition.
  */
-int add(int num1, int num2)
+int print_to_98(int num1, int num2)
 {
 	return (num1 + num2);
 }
