845504b5abd5c36c9ef324cb6ca021edc0d4331a
Enolley <enolaomukamani@gmail.com>
2022-09-15 06:00:10 -0700

    5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* more_numbers - 0 to 14 x10
+*/
+
+void more_numbers(void)
+{
+int i;
+char c
+for (i = 0; i < 9; i++)
+{
+for (c = 0; c <= 14; c++)
+{
+_putchar(c);
+}
+}
+_putchar('\n');
+}
