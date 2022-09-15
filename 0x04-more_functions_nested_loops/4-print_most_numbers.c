6fa690702aa95f520d699e2e4f30bd4240b3269b
Enolley <85625222+Enolley@users.noreply.github.com>
2022-09-15 15:21:04 +0300

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,14 @@
 #include "main.h"
 
 /**
-* print_most_numbers - print 01356789
+* print_most_numbers -  prints the numbers except 2 and 4.
 */
-
 void print_most_numbers(void)
 {
+	char c;
 
-char c;
-for (c = 0; c <= 9; c++)
-if (c != 2 && c!=4)
-_putchar(c);
-_putchar('\n');
+	for (c = '0'; c <= '9'; c++)
+		if (c != '2' && c != '4')
+			_putchar(c);
+	_putchar('\n');
 }
