1ca8412a509f05172466613792c8da8621b5c6b2
Enolley <enolaomukamani@gmail.com>
2022-09-15 06:47:42 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_line - prints a line
+* @n: number of times to print
+*/
+
+void print_line(int n)
+{
+char c;
+for (c = 0; c < n; c++)
+_putchar('_');
+_putchar('\n');
+}
