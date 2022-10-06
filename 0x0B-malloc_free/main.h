d17cc969f27b3e87c09103ba8cc52c7a1db76202
Enolley <85625222+Enolley@users.noreply.github.com>
2022-10-06 18:33:56 +0300

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,12 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * Auth: Jan Nelson
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x0A-malloc_free directory.
- */
+
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
