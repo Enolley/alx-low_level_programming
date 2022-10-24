e7d042e9903cb0865f6781fb118310aca24321af
Enolley <85625222+Enolley@users.noreply.github.com>
2022-10-24 22:32:12 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,3 @@
-/*
- * File: 0-print_list.c
- * Auth: Jan Nelson
- */
-
 #include "lists.h"
 #include <stdio.h>
 
@@ -15,19 +10,15 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
-
-	while (h)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
-		h = h->next;
-	}
-
-	return (nodes);
+size_t nodes = 0;
+while (h)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", h->len, h->str);
+nodes++;
+h = h->next;
+}
+return (nodes);
 }
